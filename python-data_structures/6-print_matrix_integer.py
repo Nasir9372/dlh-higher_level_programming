0184ee6bb42e3884b297b15c6b2e4b5e767a6630
Nasir9372 <nasirkhan9372@gmail.com>
2026-04-30 23:41:07 +0000

    printing integer in matrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for integer in range(len(row)):
+            if integer != len(row) - 1:
+                print("{:d}".formate(row[integer]), end="")
+            else:
+                print("{:d}".format(row[integer]), end="")
+        print()
