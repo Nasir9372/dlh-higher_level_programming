4a4c98fc7196c8bcb6372810f4b2c8145e0712f6
Nasir9372 <nasirkhan9372@gmail.com>
2026-05-03 16:42:47 +0000

    task 11, delete element at specific position, task can be done in another way where we dont need to create new list

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    for idx in range(len([my_list]):
+        if i != idx:
+            new_list.append(my_list[i])
+    my_list[:] = new_list
+    return my_list
+    if idx < 0 or idx >= len(my_list):
+        return my_list
