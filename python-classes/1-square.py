54733d33bfe469ee76ec5cec79ad2ee616efb482
Nasir9372 <nasirkhan9372@gmail.com>
2026-05-09 17:47:31 +0000

    square by size

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module defines the class Square."""
+
+
+class Square:
+    """This is documentation of class Square by size."""
+    def __init__(self, size):
+        self.__size = size
