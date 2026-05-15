b37b2e748aaf66e20d263a1f404b51cd40b9bdbc
Nasir9372 <nasirkhan9372@gmail.com>
2026-05-15 00:59:53 +0000

    import sys module, similar to input(), use of len,if/eles,looping,formate string to print each argument in one line

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    argument = sys.argv
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} argument:".format(count))
+
+if count >= 1:
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
