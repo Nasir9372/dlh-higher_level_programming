18c2086c39fa6f77744cfe9d7cfbb50778dde50f
Nasir9372 <nasirkhan9372@gmail.com>
2026-05-26 17:38:33 +0000

    weight average (total = each s*w, and weight = sum all w and return total/weight

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -4,7 +4,7 @@ def weight_average(my_list=[]):
         return 0
     total = 0
     weight_sum = 0
-    for score, weight in my_list
+    for score, weight in my_list:
         total += score * weight
         weight_sum += weight
     return total / weight_sum
