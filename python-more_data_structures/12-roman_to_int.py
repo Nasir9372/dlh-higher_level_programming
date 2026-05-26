cf4dfb75760bf0d0f0a431aeae32d700f219705e
Nasir9372 <nasirkhan9372@gmail.com>
2026-05-26 17:19:43 +0000

    update logic to convert roman integer

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string is None:
+    roman = {
+        'I': 1, 'V': 5, 'X': 10,
+        'L': 50, 'C': 100, 'D': 500, 'M': 1000
+    }
+
+    if roman_string is None or type(roman_string) is not str:
         return 0
-    roman_values = {
-            'I': 1, 'V': 5, 'X': 10,
-            'L': 50, 'C': 100,
-            'D': 500, 'M': 1000
-            }
+
     total = 0
-    prev_value = 0
-    for char in reversed(roman_string):
-        value = roman_values.get(char, 0)
-        if value < prev_value:
-            total -= value
+
+    for i in range(len(roman_string)):
+        c_v = roman[roman_string[i]]
+
+        # check next value if it exists
+        if i + 1 < len(roman_string):
+            n_v = roman[roman_string[i + 1]]
+
+            if c_v < n_v:
+                total -= c_v
+            else:
+                total += c_v
         else:
-            total += value
-        prev_value = value
+            # last character always add
+            total += c_v
+
     return total
